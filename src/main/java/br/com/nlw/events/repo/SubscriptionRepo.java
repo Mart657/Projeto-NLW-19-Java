2176e927cfb5f7d00dae17ee45f4d951ef4148a3
Pedro Martim <ped_titotto@hotmail.com>
2025-02-23 23:21:09 -0300

    Created if condition for all cases

--- src/main/java/br/com/nlw/events/repo/SubscriptionRepo.java
+++ src/main/java/br/com/nlw/events/repo/SubscriptionRepo.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
+import br.com.nlw.events.model.User;
 
 public interface SubscriptionRepo extends CrudRepository<Subscription, Integer> {
-
+    public Subscription findByEventAndSubscriber(Event evt, User user);
 
 
 
